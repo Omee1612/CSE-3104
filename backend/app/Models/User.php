8cacfda7cd32cfd3719ac86e0cc3e1d5ae456b6b
unknown <ebolachan2233@gmail.com>
2026-08-20 03:01:54 +0600

    Added database integration with backend logic

---
+++ backend/app/Models/User.php
@@ -0,0 +1,47 @@
+<?php
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
+
+class User extends Authenticatable
+{
+    use HasApiTokens, HasFactory, Notifiable;
+
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+        'role',
+        'phone',
+        'tracking_id',
+        'destination',
+        'agency',
+    ];
+
+    protected $hidden = ['password', 'remember_token'];
+
+    protected function casts(): array
+    {
+        return ['password' => 'hashed'];
+    }
+    public function contracts()
+    {
+        return $this->hasMany(Contract::class);
+    }
+    public function documents()
+    {
+        return $this->hasMany(Document::class);
+    }
+    public function postedJobs()
+    {
+        return $this->hasMany(JobListing::class, 'creator_id');
+    }
+
+    public function applications()
+    {
+        return $this->hasMany(JobApplication::class, 'applicant_id');
+    }
+}
